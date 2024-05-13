cf736327b4d743d096c95f9f7ea06230aa6beac5
ashikalahi546 <144088399+ashikalahi546@users.noreply.github.com>
2024-05-13 17:40:11 +0600

    post data

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lbnjpl6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +32,33 @@ app.get ('/create',async(req,res)=>{
   const result = await createAssignment.find().toArray()
   res.send(result)
 })
+
+/// single id jonno
+app.get('/create/:id', async(req,res)=>{
+  const id = req.params.id
+   const cursor = { _id:new ObjectId(id) }
+     const service = await createAssignment.findOne(cursor)
+        res.send(service)
+})
+
+//post data
+app.post('/create',async(req,res)=>{
+  const assignment = req.body;
+  console.log(assignment)
+  const result = await createAssignment.insertOne(assignment);
+  res.send(result)
+})
+
+
+
+    // delete
+    app.delete('/create/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+    const result = await createAssignment.deleteOne(query)
+    res.send(result)
+    })
+
 
 
     console.log(
