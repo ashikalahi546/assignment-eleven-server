025407ccb644e49dbc875684ff5af9cb40b7314a
ashikalahi546 <144088399+ashikalahi546@users.noreply.github.com>
2024-05-14 16:49:32 +0600

    port change

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
-require('dotenv').config()
+const port = process.env.PORT ||8360;
+require("dotenv").config();
 
 // middleware
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://react-email-form.web.app",
+//       "https://react-email-form.firebaseapp.com",
+//     ],
+//     credentials: true,
+//   })
+// );
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:'20MB'}));
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lbnjpl6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -22,64 +32,58 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const createAssignment = client.db("crud11").collection("create");
 
-const createAssignment = client.db('crud11').collection('create');
+    // get all jobs  data from db
 
+    app.get("/create", async (req, res) => {
+      const result = await createAssignment.find().toArray();
+      res.send(result);
+    });
 
-// get all jobs  data from db
+    /// single id jonno
+    app.get("/create/:id", async (req, res) => {
+      const id = req.params.id;
+      const cursor = { _id: new ObjectId(id) };
+      const service = await createAssignment.findOne(cursor);
+      res.send(service);
+    });
 
-app.get ('/create',async(req,res)=>{
-  const result = await createAssignment.find().toArray()
-  res.send(result)
-})
-
-/// single id jonno
-app.get('/create/:id', async(req,res)=>{
-  const id = req.params.id
-   const cursor = { _id:new ObjectId(id) }
-     const service = await createAssignment.findOne(cursor)
-        res.send(service)
-})
-
-//post data
-app.post('/create',async(req,res)=>{
-  const assignment = req.body;
-  console.log(assignment)
-  const result = await createAssignment.insertOne(assignment);
-  res.send(result)
-})
-
-
+    //post data
+    app.post("/create", async (req, res) => {
+      const assignment = req.body;
+      console.log(assignment);
+      const result = await createAssignment.insertOne(assignment);
+      res.send(result);
+    });
 
     // delete
-    app.delete('/create/:id',async(req,res)=>{
+    app.delete("/create/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-    const result = await createAssignment.deleteOne(query)
-    res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await createAssignment.deleteOne(query);
+      res.send(result);
+    });
 
-    //all update 
-    app.put('/create/:id',async(req,res)=>{
+    //all update
+    app.put("/create/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const updateAll = req.body;
       const update = {
-        $set:{
-        title:updateAll.title,
-        image:updateAll.image,
-        level:updateAll.level,
-        deadline:updateAll.deadline,
-        mark:updateAll.mark,
-        description:updateAll.description,
-        }
-      }
+        $set: {
+          title: updateAll.title,
+          image: updateAll.image,
+          level: updateAll.level,
+          deadline: updateAll.deadline,
+          mark: updateAll.mark,
+          description: updateAll.description,
+        },
+      };
 
-      const result = await createAssignment.updateOne(filter,update)
-      res.send(result)
-    })
-
-
+      const result = await createAssignment.updateOne(filter, update);
+      res.send(result);
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
