46338d6a03c995ecabad447762f116081a64f443
ashikalahi546 <144088399+ashikalahi546@users.noreply.github.com>
2024-05-18 13:18:12 +0600

    same update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser')
+
 const app = express();
 const port = process.env.PORT ||8360;
 require("dotenv").config();
